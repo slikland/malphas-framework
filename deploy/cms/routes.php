ba963c030d684d3f2b1dc431b73082761f84e190
Gabriel Silva <gabriel.silva@Sliklands-iMac.local>
2018-09-11 13:07:38 -0300

    silex removed

--- deploy/cms/routes.php
+++ deploy/cms/routes.php
@@ -1,22 +1,28 @@
 <?php
 
-    $app = new Silex\Application();
-    $app['debug'] = true;
+die('silex removed');
 
-    $app['user.controller'] = function() {
-        return new controller\User;
-    };
-
-    $app->get('/', function() use ($app) {
-        return $app['user.controller']->authenticate();
-    });
-
-    $app->get('/dashboard/user/index', function () use ($app) {
-        return $app['user.controller']->index();
-    });
-
-    $app->get('/hello/{name}', function ($name) use ($app) {
-        return '<h1>Example</h1> <p>Hello '.$app->escape($name) . '</p>';
-    });
-
-    $app->run();
+//    route /
+//
+//
+//
+//    $app = new Silex\Application();
+//    $app['debug'] = true;
+//
+//    $app['user.controller'] = function() {
+//        return new controller\User;
+//    };
+//
+//    $app->get('/', function() use ($app) {
+//        return $app['user.controller']->authenticate();
+//    });
+//
+//    $app->get('/dashboard/user/index', function () use ($app) {
+//        return $app['user.controller']->index();
+//    });
+//
+//    $app->get('/hello/{name}', function ($name) use ($app) {
+//        return '<h1>Example</h1> <p>Hello '.$app->escape($name) . '</p>';
+//    });
+//
+//    $app->run();
--- deploy/cms/vendor/composer/autoload_namespaces.php
+++ deploy/cms/vendor/composer/autoload_namespaces.php
@@ -7,5 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Twig_' => array($vendorDir . '/twig/twig/lib'),
-    'Pimple' => array($vendorDir . '/pimple/pimple/src'),
 );
