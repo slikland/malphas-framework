40a21d183c8a9407f1614b7048f3aee62422eacd
diego.sanches <diego.sanches@Diegos-iMac.local>
2018-09-21 12:07:17 -0300

    functions helper for assets in views

--- deploy/cms/view/helpers.php
+++ deploy/cms/view/helpers.php
@@ -1,5 +1,26 @@
 <?php
 
+
 function inc($path) {
     require_once VIEWS_PATH . $path;
+}
+
+function asset($file) {
+    return VIEWS_PATH . '_assets/' . $file;
+}
+
+function css($file) {
+    return VIEWS_PATH . '_assets/css/' . $file. '.css';
+}
+
+function js($file) {
+    return VIEWS_PATH . '_assets/js/' . $file . '.js';
+}
+
+function image($file) {
+    return VIEWS_PATH . '_assets/image/' . $file;
+}
+
+function title() {
+    return APP_NAME;
 }
