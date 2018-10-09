ffda7086a8e8dcb10d2ccefc4203d54b4b685bec
Gabriel Silva | Slikland <gabriel.silva@slikland.com>
2018-10-09 19:22:39 -0300

    pdo required

--- deploy/api/app/config/requirement.php
+++ deploy/api/app/config/requirement.php
@@ -5,3 +5,7 @@
     if (version_compare(phpversion(), $phpMinVersion, '<')) {
         die("PHP >= $phpMinVersion is Required");
     }
+
+    if (!extension_loaded('pdo')) {
+        die('PDO is Required');
+    }
